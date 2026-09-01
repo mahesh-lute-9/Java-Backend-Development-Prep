8b9cb060ec0effcbafdefb2982a5f81da40fc1c3
maheshl-9 <maheshlute2005@gmail.com>
2026-09-01 13:20:41 +0530

    learned how to create a spring boot project manually via adding dependency in pom.xml

---
+++ 08_SpringBootCore/SpringBootCoreDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+// creating a spring boot project manually by adding the dependency in pom.xml
