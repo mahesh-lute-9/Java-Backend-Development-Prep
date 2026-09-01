315f0a387e170d1559a0d08be4c04d112c0813b2
maheshl-9 <maheshlute2005@gmail.com>
2026-09-01 13:25:31 +0530

    created SpringBoot Project using Spring Initializr

---
+++ 08_SpringBootCore/SpringBootCoreDemo1/src/main/java/com/example/SpringBootCoreDemo1/SpringBootCoreDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCoreDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoreDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCoreDemo1Application.class, args);
+	}
+
+}
---
+++ 08_SpringBootCore/SpringBootCoreDemo1/src/test/java/com/example/SpringBootCoreDemo1/SpringBootCoreDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootCoreDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCoreDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
