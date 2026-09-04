0cedd02bfe25dbe48f071018b1f12ef6e35de2dd
maheshl-9 <maheshlute2005@gmail.com>
2026-09-04 06:04:05 +0530

    create a SpringBoot CRUD project from Spring initializr with proper dependencies

---
+++ 10_SpringBoot_CRUD/crudSpringBootDemo/src/main/java/com/example/crudSpringBootDemo/CrudSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudSpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootDemoApplication.class, args);
+	}
+
+}
